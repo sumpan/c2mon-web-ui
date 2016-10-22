18ff40f58f72014e2be4ad9e004b3d9361b4c3c6
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-10-23 01:59:58 +0200

    Clean up

--- src/main/java/cern/c2mon/web/configviewer/Application.java
+++ src/main/java/cern/c2mon/web/configviewer/Application.java
@@ -1,21 +1,18 @@
 package cern.c2mon.web.configviewer;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Justin Lewis Salmon
  */
 @SpringBootApplication
-//@MapperScan("cern.c2mon.web.configviewer.statistics.daqlog")
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     new SpringApplicationBuilder().bannerMode(Banner.Mode.OFF).sources(Application.class).run(args);
   }
 }
